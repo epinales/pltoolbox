6827b3f97e62de484f8f1778af6422d396be4196
Eduardo Santos <e.santosancira@gmail.com>
2019-06-25 19:33:30 -0500

    mayoral tools

--- Resources/PHP/DataBases/conexion.php
+++ Resources/PHP/DataBases/conexion.php
@@ -1,10 +1,18 @@
 <?php
 
-$datab = 'bitacoraProlog';
+/*TEST ENVIRONMENT*/
+$datab = 'pltoolbox';
 $host = 'localhost';
 $port = 8886;
 $usr = 'root';
 $pwd = 'root';
+
+/*PRODUCTION ENVIRONMENT*/
+// $datab = 'pltoolbox';
+// $host = 'localhost';
+// $port = 8886;
+// $usr = 'root';
+// $pwd = 'root';
 
 $db = new mysqli($host, $usr, $pwd, $datab, $port) or die ('Could not connect to the database server ' . $login->error );
 
