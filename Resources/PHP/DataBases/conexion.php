739c79b961e610390aa570ee98436f97946221c7
Eduardo Santos <e.santosancira@gmail.com>
2019-08-13 16:09:00 -0500

    Se inicio el programa para capturar y monitorear los HUs.

--- Resources/PHP/DataBases/conexion.php
+++ Resources/PHP/DataBases/conexion.php
@@ -7,11 +7,11 @@ $usr = 'root';
 $pwd = 'root';
 
 /*PRODUCTION ENVIRONMENT*/
-$datab = 'pltoolbox';
-$host = '10.1.4.10';
-$port = 3306;
-$usr = 'prolog';
-$pwd = 'f4Tnps.03';
+// $datab = 'pltoolbox';
+// $host = '10.1.4.10';
+// $port = 3306;
+// $usr = 'prolog';
+// $pwd = 'f4Tnps.03';
 
 $db = new mysqli($host, $usr, $pwd, $datab, $port) or die ('Could not connect to the database server ' . $login->error );
 
---
+++ hus/barraNavegacion.php
@@ -0,0 +1,29 @@
+<?php
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  require $root . '/pltoolbox/Resources/PHP/Utilities/initialScript.php';
+
+  date_default_timezone_set('America/Mexico_City');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HU's Management</title>
+
+  <!--***************ESTILOS*****************-->
+<link rel="stylesheet" href="/pltoolbox/Resources/css/bitacoraProlog.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/css/estilos.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/css/barraNavegacion.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/css/modales.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/css/estilosBootstrap.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/Bootstrap_4_3/css/bootstrap.min.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/sweetAlert/css/sweetalert.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/alertify/css/alertify.min.css">
+<link rel="stylesheet" href="/pltoolbox/Resources/alertify/css/themes/default.min.css">
+
+
+</head>
+</html>
