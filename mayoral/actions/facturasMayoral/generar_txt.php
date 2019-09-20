730935f4ec57f1178ddf42c4eb16c210e6d6337c
Eduardo Santos <e.santosancira@gmail.com>
2019-09-20 18:54:53 -0500

    brand was including a space.

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -145,7 +145,7 @@ foreach ($invoice_items as $item) {
   $marca = $item[21] == "NEWBORN" ? "MAYORAL" : $item[21];
 
   //Agregar Y DISEÑO a todas las marcas excepto NUKUTAVAKE (o si hay registros vacíos).
-  if (!($marca == 'NUKUTAVAKE' ||$marca == "")) {
+  if (!($marca == 'NUKUTAVAKE' ||$marca == "" ||$marca == " ")) {
     $marca .= " Y DISENO";
   }
 
