f203f4615b492b57bae8c243589a51e04a3e3ef8
Eduardo Santos <e.santosancira@gmail.com>
2020-08-11 12:36:03 -0500

    .

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -128,6 +128,7 @@ $num_headers = count($documented_headers);
 
 for ($i=0; $i < $num_headers; $i++) {
   if (!($headers[$i] == $documented_headers[$i])) {
+    error_log("Comparación: " . $headers[$i] . " - " . $documented_headers[$i]);
     $system_callback['code'] = 500;
     $system_callback['message'] = "Los encabezados no son correctos. Se esperaba " . utf8_encode($headers[$i]) . "; y se encontró: $documented_headers[$i].";
     error_log(mb_detect_encoding($documented_headers[$i]));
