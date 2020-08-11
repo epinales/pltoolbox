bdbd61f4bc1ce21a766eaa976027924b368e09de
Eduardo Santos <e.santosancira@gmail.com>
2020-08-11 11:25:27 -0500

    .

--- Resources/PHP/Utilities/initialScript.php
+++ Resources/PHP/Utilities/initialScript.php
@@ -25,7 +25,7 @@ $horaActual = date('H:i');
 
 include($root . '/pltoolbox/Resources/PHP/DataBases/conexion.php');
 function exit_script($input_array){
- $json_string = json_encode($input_array);
+ $json_string = json_encode($input_array, JSON_UNESCAPED_UNICODE);
  echo $json_string;
  global $db;
  $db->close();
