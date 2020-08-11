bfec7bb158830a837cf304c48efe5a32546232f3
Eduardo Santos <e.santosancira@gmail.com>
2020-08-11 12:04:56 -0500

    .

--- Resources/PHP/Utilities/initialScript.php
+++ Resources/PHP/Utilities/initialScript.php
@@ -24,13 +24,42 @@ $horaActual = date('H:i');
 
 
 include($root . '/pltoolbox/Resources/PHP/DataBases/conexion.php');
+
+function returnJsonHttpResponse($success, $data){
+    // remove any string that could create an invalid JSON
+    // such as PHP Notice, Warning, logs...
+    ob_clean();
+
+    // this will clean up any previously added headers, to start clean
+    header_remove();
+
+    // Set the content type to JSON and charset
+    // (charset can be set to something else)
+    header("Content-type: application/json; charset=utf-8");
+
+    // Set your HTTP response code, 2xx = SUCCESS,
+    // anything else will be error, refer to HTTP documentation
+    if ($success) {
+        http_response_code(200);
+    } else {
+        http_response_code(500);
+    }
+
+    // encode your PHP Object or Array into a JSON string.
+    // stdClass or array
+    echo json_encode($data);
+
+    // making sure nothing is added
+    exit();
+}
+
 function exit_script($input_array){
- header('Content-Type: application/; charset=utf-8');
- $json_string = json_encode($input_array);
- echo $json_string;
- global $db;
- $db->close();
- die();
+ // $json_string = json_encode($input_array);
+ returnJsonHttpResponse(true, $input_array);
+ // echo $json_string;
+ // global $db;
+ // $db->close();
+ // die();
 
 }
 
