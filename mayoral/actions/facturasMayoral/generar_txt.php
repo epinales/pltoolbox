d8e10be831207fde1289191c33261f7c44aab492
Eduardo Santos <e.santosancira@gmail.com>
2020-08-11 11:26:57 -0500

    .

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -107,7 +107,7 @@ $documented_headers = ["﻿AÑO","FACTURA","ARTICULO","PIEZA","RANGO","DESCRIPCI
 
 if (!$test_extension) {
   $system_callback['code'] = 500;
-  $system_callback['message'] = htmlspecialchars("Los archivos con extensión $extension no están permitidos. Favor de subir un archivo CSV.");
+  $system_callback['message'] = utf8_encode("Los archivos con extensión $extension no están permitidos. Favor de subir un archivo CSV.");
   exit_script($system_callback);
 }
 
@@ -124,7 +124,7 @@ $num_headers = count($documented_headers);
 for ($i=0; $i < $num_headers; $i++) {
   if (!($headers[$i] == $documented_headers[$i])) {
     $system_callback['code'] = 500;
-    $system_callback['message'] = htmlspecialchars("Los encabezados no son correctos. Se esperaba $headers[$i]; y se encontró: $documented_headers[$i].");
+    $system_callback['message'] = utf8_encode("Los encabezados no son correctos. Se esperaba $headers[$i]; y se encontró: $documented_headers[$i].");
     exit_script($system_callback);
   }
 }
