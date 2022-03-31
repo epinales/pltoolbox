e7583aba195c2f1187641b39c1a9202823253b58
Eduardo Santos <e.santosancira@gmail.com>
2022-03-31 15:46:31 -0500

    cnt_20

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -4691,12 +4691,12 @@ $muestrario = [
   ['10322124','95030012','9166, 9167, 19168'],
 ];
 
-$folios_file = $root . "/pltoolbox/mayoral/resources/TempFiles/items_sin_folios_cnt_19.csv";
+$folios_file = $root . "/pltoolbox/mayoral/resources/TempFiles/items_sin_folios_cnt_20.csv";
 $folios_file = fopen($folios_file, "w");
 
 fputcsv($folios_file, ['Linea', 'Fraccion', 'Modelo']);
 
-foreach ($cnt_19 as $uva){
+foreach ($cnt_20 as $uva){
     $folio = $uva[0];
     $fraccion = $uva[1];
     $modelos = explode(', ', $uva[2]);
