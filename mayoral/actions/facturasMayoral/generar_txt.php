927b2e0f9c38ce5341de24ad1a8efd7774676bd7
Eduardo Santos <e.santosancira@gmail.com>
2022-09-05 10:06:27 -0500

    cnt_18

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -4035,7 +4035,7 @@ $folios_file = fopen($folios_file, "w");
 
 fputcsv($folios_file, ['Linea', 'Fraccion', 'Modelo']);
 
-foreach ($cnt_17 as $uva){
+foreach ($cnt_18 as $uva){
     $folio = $uva[0];
     $fraccion = $uva[1];
     $modelos = explode(',',str_replace(' ', '', $uva[2]));
