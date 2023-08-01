708a4889eb0a8470c1c7596441d87efcf53a5328
Estefania Pinales <ae_pinales@hotmail.com>
2023-08-01 11:12:27 -0600

    cnt_A23001526

--- mayoral/actions/facturasMayoral/generar_txt.php
+++ mayoral/actions/facturasMayoral/generar_txt.php
@@ -1568,7 +1568,7 @@ $folios_file = fopen($folios_file, "w");
 
 fputcsv($folios_file, ['Linea', 'Fraccion', 'Modelo']);
 
-foreach ($cnt_3_V23000251 as $uva){
+foreach ($cnt_A23001526 as $uva){
     $folio = $uva[0];
     $fraccion = $uva[1];
     $modelos = explode(',',str_replace(' ', '', $uva[2]));
